c920d3100ae891f3b5ed86e21de519cce010d8ea
a1allen <144809246+a1allen@users.noreply.github.com>
2023-11-20 15:05:35 -0500

    Revert "Boskos awesome branch"

--- ManagementFAQControl.vb
+++
@@ -1,5 +0,0 @@
-﻿Public Class ManagementFAQControl
-    Private Sub BackButton_Click(sender As Object, e As EventArgs) Handles BackButton.Click
-        Form1.showManagementTab()
-    End Sub
-End Class
